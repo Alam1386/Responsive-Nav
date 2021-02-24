9b35cd416d38afe9a061fa34e9ab1b291fc7f73e
Alam Talash <alamtalash@Alams-MBP.hitronhub.home>
2021-02-23 21:56:54 -0800

    animation is added on scroll event

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const humburger = document.querySelector('.humburger');
-const navBar = document.querySelector('.nav-links');
+let humburger = document.querySelector('.humburger');
+let navLinks = document.querySelector('.nav-links');
+let navBar = document.querySelector('.nav-bar');
+const header = document.querySelector('.header');
 humburger.addEventListener('click', ()=>{
   console.log("Clicked")
-  navBar.classList.toggle('active'); 
+ navLinks.classList.toggle('active'); 
 })
+let transparent = ()=>{
+  if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+    navBar.style.cssText = "opacity: 0.8; padding: 0;";
+  } else {
+    navBar.style.cssText = "opacity: 1; padding: 10px;";
+
+  }
+}
+// document.addEventListener('scroll', transparent);
+window.onscroll = function() {transparent();}
